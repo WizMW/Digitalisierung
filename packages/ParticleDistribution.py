e08bcc7e8e9e1eb2fc08748adc245685bda95647
Merlin W <Merlinwolske@gmail.com>
2024-02-12 21:09:54 +0100

    patch: Bug in Q3 distribution

--- packages/ParticleDistribution.py
+++ packages/ParticleDistribution.py
@@ -12,31 +12,47 @@ class Distribution:
         self.q3 = None
         self.Q3 = None
         self.data_area = data_img.dropna().to_numpy()
+        self.bins = None 
 
     def PGV(self, hist_size = 1000):
-        diameter = np.sqrt(self.data_area * 4/np.pi)
-        self.q0, bin_edgesq0 = np.histogram(diameter, hist_size)
+        diameter = np.sqrt(self.data_area/np.pi)
+        mass = 4/3 * np.pi * ((diameter/2)**3)
+        self.q0, xi = np.histogram(diameter, hist_size)
         self.Q0 = np.cumsum(self.q0) / np.sum(self.q0)
-        mass = 4/3 * np.pi * np.square(diameter/2)
-        self.q3, bin_edgesq3  = np.histogram(mass, hist_size)
+        # Q3 Berechnung
+        x_mean = (xi + np.roll(xi,-1)) / 2
+        x_mean = x_mean[:-1]
+        self.q3 = ((x_mean**3) * self.q0)/np.sum((x_mean**3) * self.q0 * (xi[1]-xi[0]))
         self.Q3 = np.cumsum(self.q3) / np.sum(self.q3)
-        return bin_edgesq0, bin_edgesq3
+        # Histogramm plotten
+        self.bins = np.linspace(0, np.max(diameter), len(self.q3))
 
     def fit_weibull(self):
-        if self.Q3 is None:
-            self.PGV()
-        # Define the cumulative probabilities
-        x = np.arange(len(self.Q3))
-
         # Define the Weibull cumulative distribution function
         def weibull_cdf(x, c, loc, scale):
             return weibull_min.cdf(x, c, loc=loc, scale=scale)
 
+        if self.Q3 is None:
+            self.PGV()
+        # Define the cumulative probabilities
+        x = self.bins
         # Curve fitting using Least Squares Method (LSM)
-        params, _ = curve_fit(weibull_cdf, x, self.Q3)
+        params, _ = curve_fit(weibull_cdf, x, self.Q3, p0 = [1, 0, 1])
         # Extract the estimated parameters
         shape, loc, scale = params
         # Return the estimated parameters
+        # plt.plot(self.bins, self.Q3, color='blue',label='$Q_3$ Verteilung')
+        # plt.plot(self.bins, weibull_min.cdf(x, shape, loc=loc, scale=scale), color='red', label='Weibull-Fit')
+        # plt.legend()
+        # plt.xlabel('Durchmesser (mm)',fontsize=20)
+        # plt.ylabel('Massenanteil ($m_i$/m)',fontsize=20)
+        # plt.title('$Q_3$ Verteilung',fontsize=25)
+        # plt.grid(True)
+        # plt.show()
+
+
+
+
         return shape, loc, scale
 
 df = pd.read_csv('exp\Area.csv')
@@ -52,41 +68,28 @@ df = pd.read_csv('exp\Area.csv')
 
 
 
-
-
-
 # data_here = df['10%_0']
 # Dist = Distribution(data_here)
-# binedges0, binedges3 = Dist.PGV()
-# # Histogramm erstellen basierend auf self.q0
-# plt.hist(Dist.q0, bins=len(Dist.q0), color='skyblue', edgecolor='black')
-
-# # Titel und Beschriftungen hinzufügen
-# plt.title('q0')
-# plt.xlabel('Partikelgröße (m)')
-# plt.ylabel('Häufigkeit')
-
-# # Histogramm anzeigen
-# plt.show()
+# Dist.PGV()
 
 
 
 
-# column_names = df.columns.tolist()
-# Weibull_data = pd.DataFrame()
-# speeds = np.arange(10, 101, 10)
+column_names = df.columns.tolist()
+Weibull_data = pd.DataFrame()
+speeds = np.arange(10, 101, 10)
 
-# for speed in speeds:
-#     i = 0
-#     while f'{str(speed)}%_{str(i)}' in df.columns:
-#         Dist = Distribution(df[f'{str(speed)}%_{str(i)}'])
-#         shape, loc, scale = Dist.fit_weibull()
-#         index = len(Weibull_data)
-#         Weibull_data.loc[index, 'shape'] = shape
-#         Weibull_data.loc[index, 'loc'] = loc
-#         Weibull_data.loc[index, 'scale'] = scale
-#         i += 1
+for speed in speeds:
+    i = 0
+    while f'{str(speed)}%_{str(i)}' in df.columns:
+        Dist = Distribution(df[f'{str(speed)}%_{str(i)}'])
+        shape, loc, scale = Dist.fit_weibull()
+        index = len(Weibull_data)
+        Weibull_data.loc[index, 'shape'] = shape
+        Weibull_data.loc[index, 'loc'] = loc
+        Weibull_data.loc[index, 'scale'] = scale
+        i += 1
 
-# Weibull_data["Names"] = column_names
-# Weibull_data = Weibull_data.set_index('Names')
-# Weibull_data.to_csv('Weibull_dist.csv')
+Weibull_data["Names"] = column_names
+Weibull_data = Weibull_data.set_index('Names')
+Weibull_data.to_csv('Weibull_dist.csv')
